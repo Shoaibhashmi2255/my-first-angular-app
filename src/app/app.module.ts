c59c8bc0bad7fc15f2ba5de0256b7d7ee786968a
Shoaib <shoaibzafar2255@gmail.com>
2022-05-17 14:38:03 +0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FotterComponent } from './fotter/fotter.component';
+import { HomeComponent } from './home/home.component';
+import { WalletComponent } from './wallet/wallet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FotterComponent,
+    HomeComponent,
+    WalletComponent
   ],
   imports: [
     BrowserModule,
